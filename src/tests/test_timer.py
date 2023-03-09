5f132df416b3cc2f828fc3716ba09a5c5a58fb7a
Mateusz Bagiński <m.baginski@optimumpareto.com>
2023-03-09 09:48:11 +0100

    removed everything except Timer and documented Timer

--- src/tests/test_timer.py
+++ src/tests/test_timer.py
@@ -3,7 +3,7 @@ import logging
 
 import pytest
 
-from thyming.timer import NoLoggerError, Timer, TimerAlreadyRunningError, TimerNotRunningError, Timestamp
+from thyming.timer import Timer, TimerAlreadyRunningError, TimerNotRunningError, DefaultMessage
 
 def test_100ms():
     t = Timer()
--- src/thyming/__init__.py
+++ src/thyming/__init__.py
@@ -1,4 +1,1 @@
-from thyming.asyncio_time_run import asyncio_time_run
-from thyming.time_diff import time_diff
-from thyming.time_func import time_func
-from thyming.timer import Timer, Timestamp
+from thyming.timer import Timer, DefaultMessage
